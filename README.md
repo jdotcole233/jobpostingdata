b5a4c8ba805d03fae98e86c8cfdc48c7cb20262a
jdotcole233 <jdotcole233@gmail.com>
2021-07-01 10:17:27 +0000

    instructions

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+Data set extracted from https://data.world/
+This is for educational purposes only.
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+// 1. The data included in this  project was extracted from https://data.world/
+// 2.  But the actual source is from https://www.governmentjobs.com/careers/raleighnc
+// 3.  Your task to study how the data has been displayed in 2 above and replicate same structure in this prohect
+// 4. It should be a console app
